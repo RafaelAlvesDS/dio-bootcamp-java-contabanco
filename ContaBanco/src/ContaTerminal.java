09a46c4cd6092c83c0547890002f3c2df1a1a1d3
Rafael Alves <rafael.alves.ds@gmail.com>
2025-07-23 01:30:38 -0300

    Lógica do desafio feita

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        var scanner = new Scanner(System.in);
+        System.out.println("Digite o número da conta: ");
+        var numero = scanner.nextInt();
+
+        System.out.println("Digite o número e digito da agência: ");
+        var agencia = scanner.next();
+
+        System.out.println("Digite o nome do cliente: ");
+        var nomeCliente = scanner.next();
+
+        System.out.println("Digite o valor a ser depositado: ");
+        var saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+        scanner.close();
     }
 }
